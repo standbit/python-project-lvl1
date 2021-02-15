c3168e12ed1cd5d2d019f00286b90136958c7940
Stanislav <yastva3@gmail.com>
2021-02-15 21:54:36 +0300

    do 3 step, renamed brain_gamess, add new mod

---
+++ brain_games/scripts/brain_gamess.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from brain_games import cli
+
+def greet():
+    print('Welcome to the Brain Games!')
+
+def main():
+    greet()
+    cli.welcome_user()
+
+if __name__ == '__main__':
+    main()
+ 
